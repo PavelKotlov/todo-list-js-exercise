613bca4e77f4342338bdb09727edbf1a5f008b95
PavelKotlov <pavelkotlov@gmail.com>
2023-02-17 15:01:20 -0800

    Refactored index to be more object oriented.

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logTaskState: function () {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    completeTask: function () {
+      this.complete = true;
+    }
+
+
+  }
+
+  return task;
 }
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
+const task1 = newTask("Clean Cat Litter", "If litter is overly dirty, dispose of all the litter and pour new litter into the litter box. Else scoop the cat doings into a trash bag and dispose.");
 
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
+const task2 = newTask("Do Laundry", "Place dirty garments into the washing machine. Add detergent, set desired cycle, and push start. Once washing is complete, transfer the wet articles into the dryer. Now, select the desired cycle and push start. Upon completion retrieve and fold the articles and put away.");
+ // task 1
 
-// DRIVER CODE BELOW
+const tasks = [task1, task2]
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+task1.logTaskState(task1); // Clean Cat Litter has not been completed
+task1.completeTask(task1);
+task1.logTaskState(task1); // Clean Cat Litter has been completed
